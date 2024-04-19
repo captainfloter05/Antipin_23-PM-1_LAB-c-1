a58217da9be3f0da09a2ff677594ca0aa314e2ae
Roman <147859696+captainfloter05@users.noreply.github.com>
2024-04-19 18:45:47 +0300

    Update лаба1 Антипин Р 23-ПМ-1.cpp

--- лаба1 Антипин Р 23-ПМ-1.cpp
+++ лаба1 Антипин Р 23-ПМ-1.cpp
@@ -10,9 +10,11 @@ using namespace std;
 using namespace funcs;
 
 int main() {
-    int flt=0;
-    int flt1, flt2, flt3;
+    int flt, flt1, flt3;
+    int strnumber;
+    string fltname, fltmaterial; int fltper;
     int n{};
+
     cout << "n=";
     cin >> n;
     if (n < 1)
@@ -29,56 +31,33 @@ int main() {
         cin >> material;
         cout << "per[" << i << "]=";
         cin >> per;
+        cout << endl;
         arr[i].setName(name);
         arr[i].setMaterial(material);
         arr[i].setPer(per);
     }
 
-    cout << "\ncreated arr (hooray):" << endl;
+    cout << "created arr (hooray):" << endl;
     for (int i = 0; i < n; i++) {
-        cout << "name[" << i << "]=" << arr[i].name() << endl;
-        cout << "material[" << i << "]=" << arr[i].material() << endl;
-        cout << "per[" << i << "]=" << arr[i].per() << endl;
+        cout << arr[i].name() << " " << arr[i].material() << " " << arr[i].per() << endl;
     }
     save(arr, n);
 
-    while (1) {
-        cout << "1-create,2-load,3-get outta here" << endl;
-        cin >> flt1;
-        if (flt1 < 0 || flt1>3) return 0;
-        switch (flt1) {
+    cout << "1 - create, 2 - load, 3 - search, 4 - get outta here" << endl;
+    cin >> flt;
+    switch (flt) {
         case 1: {
-            cout << "1-create new arr(Furniture),2-change arr(Furniture),3-add new elements for arr(Furniture),4-delete elements from arr(Furniture)" << endl;
-            cin >> flt2;
-            switch (flt2)
-            {
-            case 1: {
-                cout << "n=";
-                cin >> n;
-                if (n < 1) return 0;
-                Furniture* arr = new Furniture[n];
-                for (int i = 0; i < n; i++) {
-                    string name;
-                    string material;
-                    int per;
-                    cout << "name[" << i << "]=";
-                    cin >> name;
-                    cout << "material[" << i << "]=";
-                    cin >> material;
-                    cout << "per[" << i << "]=";
-                    cin >> per;
-                    arr[i].setName(name);
-                    arr[i].setMaterial(material);
-                    arr[i].setPer(per);
-                    save(arr, n);
-                }
-            }
-            case 2: {
-                cout << "Which element?" << endl;
-                cin >> flt3;
-                arr = load(n);
-                for (int i = 0; i <= flt3; i++) {
-                    if (i == flt3) {
+            cout << "1 - create new arr, 2 - change arr, 3 - add new elements for arr, 4 - delete elements from arr" << endl;
+            cin >> flt1;
+            switch (flt1) {
+                case 1: {
+                    cout << "n=";
+                    cin >> n;
+                    if (n < 1)
+                        return 0;
+
+                    Furniture* arr = new Furniture[n];
+                    for (int i = 0; i < n; i++) {
                         string name;
                         string material;
                         int per;
@@ -91,134 +70,151 @@ int main() {
                         arr[i].setName(name);
                         arr[i].setMaterial(material);
                         arr[i].setPer(per);
-                        save(arr, n);
                     }
 
-                }
-            }
-            case 3: {
-                string name;
-                string material;
-                int per;
-                Furniture* arrflt = new Furniture[n + 1];
-                for (int i = 0; i < n; i++) {
-                    arrflt[i] = arr[i];
-                }
-                delete[] arr;
-                Furniture* arr = new Furniture[n + 1];
-                for (int i = 0; i < n + 1; i++) {
-                    arr[i] = arrflt[i];
-                }
-                delete[] arrflt;
-                cout << "name[" << n << "]=";
-                cin >> name;
-                cout << "material[" << n << "]=";
-                cin >> material;
-                cout << "per[" << n << "]=";
-                cin >> per;
-                arr[n].setName(name);
-                arr[n].setMaterial(material);
-                arr[n].setPer(per);
-                save(arr, n + 1);
-            }
-            case 4: {
-                cout << "Which element?" << endl;
-                cin >> flt3;
-                for (int i = 0; i < n; i++) {
-                    if (i == flt3) {
-                        Furniture* arrflt = new Furniture[n - 1];
-                        for (int i = 0; i < flt3; i++) {
-                            arrflt[i] = arr[i];
-                        }
-                        for (int i = flt3 + 1; i < n - 1; i++) {
-                            arrflt[i] = arr[i];;
-                        }
-                        delete[] arr;
-                        Furniture* arr = new Furniture[n - 1];
-                        for (int i = 0; i < n - 1; i++) {
-                            arr[i] = arrflt[i];
-                        }
-                        delete[] arrflt;
-                        save(arr, n - 1);
-                    }
-                }
-            }
-            }
-        }
-        case 2: {
-            cout << "1-load full arr(Furniture),2-find element from arr(Furniture)" << endl;
-            cin >> flt3;
-            if (flt3 < 0 || flt3>2) return 0;
-            switch (flt3) {
-            case 1: {
-                cout << "loading from (Furniture.txt): " << endl;
-                arr = load(n);
-                cout << "loaded n=" << n << endl;
-                if (!arr) {
-                    cout << "arr is null (hehehehaw)" << endl;
-                    return 0;
-                }
-                for (int i = 0; i < n; i++) {
-                    cout << "name[" << i << "]=" << arr[i].name() << endl;
-                    cout << "material[" << i << "]=" << arr[i].material() << endl;
-                    cout << "per[" << i << "]=" << arr[i].per() << endl;
-                }
-            }
-            case 2: {
-                cout << "1-name,2-material,3-period" << endl;
-                cin >> flt3;
-                if (flt3 < 0 || flt3>3) return 0;
-                switch (flt3) {
-                case 1: {
-                    string nameflt;
-                    cout << "name=" << endl;
-                    cin >> nameflt;
+                    cout << "\ncreated arr (hooray):" << endl;
                     for (int i = 0; i < n; i++) {
-                        if (arr[i].name() == nameflt) {
-                            cout << "found->" << arr[i].name() << " " << arr[i].material() << " " << arr[i].per() << endl;
-                            flt = 1;
-                        }
+                        cout << arr[i].name() << " " << arr[i].material() << " " << arr[i].per() << endl;
                     }
-                    if (flt == 0) cout << "not found (hehehehaw)" << endl;
-                    flt = 0;
-                }
+                    save(arr, n);
+                    break;
+                    }
                 case 2: {
-                    string matflt;
-                    cout << "material=" << endl;
-                    cin >> matflt;
+                    cout << "which string - ";
+                    cin >> strnumber;
+                    arr=load(n);
                     for (int i = 0; i < n; i++) {
-                        if (arr[i].material() == matflt) {
-                            cout << "found->" << arr[i].name() << " " << arr[i].material() << " " << arr[i].per() << endl;
-                            flt = 1;
-                        }
+                        if (i == strnumber) {
+                            string name;
+                            string material;
+                            int per;
+                            cout << "name[" << i << "]=";
+                            cin >> name;
+                            cout << "material[" << i << "]=";
+                            cin >> material;
+                            cout << "per[" << i << "]=";
+                            cin >> per;
+                            arr[i].setName(name);
+                            arr[i].setMaterial(material);
+                            arr[i].setPer(per);
+                            save(arr, n);
+                        } 
                     }
-                    if (flt == 0) cout << "not found (hehehehaw)" << endl;
-                    flt = 0;
+                    break;
                 }
                 case 3: {
-                    int perflt;
-                    cout << "period=" << endl;
-                    cin >> perflt;
+                    string name;
+                    string material;
+                    int per;
+                    Furniture* fltarr = new Furniture[n + 1];
                     for (int i = 0; i < n; i++) {
-                        if (arr[i].per() == perflt) {
-                            cout << "found->" << arr[i].name() << " " << arr[i].material() << " " << arr[i].per() << endl;
-                            flt = 1;
+                        fltarr[i] = arr[i];
+                    }
+                    delete[] arr;
+                    Furniture* arr = new Furniture[n + 1];
+                    for (int i = 0; i < n + 1; i++) {
+                        arr[i] = fltarr[i];
+                    }
+                    delete[] fltarr;
+                    cout << "name[" << n << "]=";
+                    cin >> name;
+                    cout << "material[" << n << "]=";
+                    cin >> material;
+                    cout << "per[" << n << "]=";
+                    cin >> per;
+                    arr[n].setName(name);
+                    arr[n].setMaterial(material);
+                    arr[n].setPer(per);
+                    save(arr, n + 1);
+                    break;
+                }
+                case 4: {
+                    cout << "Which string - ";
+                    cin >> strnumber;
+                    for (int i = 0; i < n; i++) {
+                        if (i == strnumber) {
+                            Furniture* fltarr = new Furniture[n - 1];
+                            for (int i = 0; i < strnumber; i++) {
+                                fltarr[i] = arr[i];
+                            }
+                            for (int i = strnumber + 1; i < n - 1; i++) {
+                                fltarr[i] = arr[i];
+                            }
+                            delete[] arr;
+                            Furniture* arr = new Furniture[n - 1];
+                            for (int i = 0; i < n - 1; i++) {
+                                arr[i] = fltarr[i];
+                            }
+                            delete[] fltarr;
+                            save(arr, n - 1);
                         }
                     }
-                    if (flt == 0) cout << "not found (hehehehaw)" << endl;
-                    flt = 0;
+                    break;
                 }
-                }
+                default:
+                    break;
             }
-            }
+            
         }
-        case 3: {
+        case 2: {
+            cout << "loading from (Furniture.txt): " << endl;
+            arr = load(n);
+            cout << "n=" << n << endl;
+            if (!arr) {
+                cout << "arr is null (hehehehaw)" << endl;
+                return 0;
+            }
+            for (int i = 0; i < n; i++) {
+                cout << arr[i].name()<<" "<< arr[i].material() << " " << arr[i].per() << endl;
+            }
             break;
         }
+        case 3: {
+            cout << "1 - name, 2 - material, 3 - period" << endl;
+            cin >> flt3;
+            switch (flt3) {
+                case 1: {
+                    cout << "name=";
+                    cin >> fltname;
+                    for (int i = 0; i < n; i++) {
+                        if (arr[i].name() == fltname)
+                            cout << "found->" << arr[i].name() << " " << arr[i].material() << " " << arr[i].per() << endl;
+                        else cout << "not found (hehehehaw)" << endl;
+                    }
+                    break;
+                }
+                case 2: {
+                    cout << "material=";
+                    cin >> fltmaterial;
+                    for (int i = 0; i < n; i++) {
+                        if (arr[i].material() == fltmaterial)
+                            cout << "found->" << arr[i].name() << " " << arr[i].material() << " " << arr[i].per() << endl;
+                        else cout << "not found (hehehehaw)" << endl;
+                    }
+                    break;
+                }
+                case 3: {
+                    cout << "period=";
+                    cin >> fltper;
+                    for (int i = 0; i < n; i++) {
+                        if (arr[i].per() == fltper)
+                            cout << "found->" << arr[i].name() << " " << arr[i].material() << " " << arr[i].per() << endl;
+                        else cout << "not found (hehehehaw)" << endl;
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
         }
-        delete[] arr;
-        return 0;
+        case 4: {
+            break;
+        }
+        default:
+            break;
     }
+    delete[]arr;
+    return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
